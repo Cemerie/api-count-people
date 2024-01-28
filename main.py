65e47a8c4122ca911a04b773f61b6d4808c5da6e
Patrycja Inglot <patrycja.inglot99@gmail.com>
2024-01-28 12:18:19 +0100

    count people api

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from flask import Flask
 from flask_restful import Resource, Api
+import cv2
 
 app = Flask(__name__)
 api = Api(app)
 
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+class peopleCount(Resource):
+    def get(self):
+        img=cv2.imread('images/zdj.jpg')
+        boxes, weights = hog.detectMultiScale(img, winStride=(8, 8))
+        return {'count': len(boxes)}
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
+api.add_resource(peopleCount, '/')
 api.add_resource(HelloWorld, '/test')
 
 if __name__ == '__main__':
